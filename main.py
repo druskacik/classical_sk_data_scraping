9f28530c21582beb029d2ce90162cef99b890367
Robert Druska <robert.druska@gmail.com>
2025-03-16 13:09:19 +0100

    shedule at night

--- main.py
+++ main.py
@@ -24,15 +24,10 @@ if __name__ == "__main__":
     
     print("Sheduling crawlers...")
     
-    schedule.every().minute.do(run_crawler, filharmonia_sk_main)
-    schedule.every().minute.do(run_crawler, konvergencie_sk_main)
-    schedule.every().minute.do(run_crawler, sfk_sk_main)
-    schedule.every().minute.do(run_crawler, snd_sk_main)
-    
-    # schedule.every().day.at("01:00").do(filharmonia_sk_main)
-    # schedule.every().day.at("01:01").do(konvergencie_sk_main)
-    # schedule.every().day.at("01:02").do(sfk_sk_main)
-    # schedule.every().day.at("01:03").do(snd_sk_main)
+    schedule.every().day.at("01:00").do(filharmonia_sk_main)
+    schedule.every().day.at("01:01").do(konvergencie_sk_main)
+    schedule.every().day.at("01:02").do(sfk_sk_main)
+    schedule.every().day.at("01:03").do(snd_sk_main)
     
     while True:
         schedule.run_pending()
